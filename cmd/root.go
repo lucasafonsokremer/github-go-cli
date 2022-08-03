3c9d8fb8e836dec7ea85f0df73c216a6c0a3c6e0
Lucas Afonso Kremer <lucas.kremer@softplan.com.br>
2022-08-03 13:51:49 -0300

    fix: removed version

--- cmd/root.go
+++ cmd/root.go
@@ -34,7 +34,6 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "github-go-cli",
-	Version: "${{ github.event.release.tag_name }}",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
