268e14b39c849943112a359748b966d7ff139333
Lucas Afonso Kremer <lucas.kremer@softplan.com.br>
2022-08-06 11:36:30 -0300

    feat: adicionado resource followers ao command list

--- cmd/list/followers.go
+++ cmd/list/followers.go
@@ -6,8 +6,12 @@ package list
 
 import (
 	"fmt"
+	"os"
+	"text/tabwriter"
 
 	"github.com/spf13/cobra"
+	"github.com/lucasafonsokremer/github-go-cli/cmd/util/service"
+	"github.com/lucasafonsokremer/github-go-cli/cmd/util/types"
 )
 
 // followersCmd represents the followers command
@@ -15,7 +19,29 @@ var followersCmd = &cobra.Command{
 	Use:   "followers",
 	Short: "A followers of",
 	Long: ``,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("followers called")
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) == 0 {
+			return fmt.Errorf("please, specify a username")
+		}
+		return nil
 	},
+	Run: func(cmd *cobra.Command, args []string) {
+		client := service.CreateClient(fmt.Sprintf("/users/%s/followers", args[0]))
+		followers, err := client.GetFollowers()
+		if err != nil {
+			_ = fmt.Errorf("%v", err)
+			os.Exit(1)
+		}
+
+		displayFollowers(followers)
+	},
+}
+
+func displayFollowers(followers types.Followers) {
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 5, ' ', 0)
+	_, _ = fmt.Fprintln(w, "Name\tGithub Link")
+	for _, follower := range followers {
+		_, _ = fmt.Fprintln(w, fmt.Sprintf("%s\t%s", follower.Name, follower.HTMLURL))
+	}
+	_ = w.Flush()
 }
